34dc327fe9fbfe0e3352a45c3804efe789967271
Mega-117 <enricosaian@hotmail.it>
2021-11-03 18:26:43 +0100

    milestone 1 completata

--- js/script.js
+++ js/script.js
@@ -4,6 +4,34 @@ window.addEventListener("DOMContentLoaded", function () {
     const vueApp = new Vue({
         el: "#app",
         data: {
+            listaChat: [
+                {
+                    nome: "Mario Rossi",
+                    avatar: "img/avatar_1.jpg",
+                    messaggio: "ciao",
+                    dataUltimoMessaggio: "10",
+                },
+                {
+                    nome: "Riccardo Mengoni",
+                    avatar: "img/avatar_1.jpg",
+                    messaggio: "ciao",
+                    dataUltimoMessaggio: "10",
+                },
+                {
+                    nome: "Paolo Fontana",
+                    avatar: "img/avatar_1.jpg",
+                    messaggio: "ciao",
+                    dataUltimoMessaggio: "10",
+                },
+                {
+                    nome: "Michele Scotti",
+                    avatar: "img/avatar_1.jpg",
+                    messaggio: "ciao",
+                    dataUltimoMessaggio: "10",
+                },
+
+            ],
+            newMessage: ""
 
         }
     });
