990be05067bbf50f865a510056ee16fce2a5b2de
Mega-117 <enricosaian@hotmail.it>
2021-11-04 14:49:39 +0100

    messo funz che cambia le chat al click

--- Milestone-2/js/script.js
+++ Milestone-2/js/script.js
@@ -6,73 +6,107 @@ window.addEventListener("DOMContentLoaded", function () {
         data: {
             listaChat: [
                 {
-                    nome: "Mario Rossi",
-                    avatar: "img/avatar_1.jpg",
-                    ultimoMessaggio: "ciao come stai ?",
-                    dataUltimoMessaggio: "10/08/2021 16:35",
-                    id: "chatUtente1"
+                    name: 'Michele',
+                    avatar: '_1',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Ricordati di dargli da mangiare',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            text: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
                 },
                 {
-                    nome: "Riccardo Mengoni",
-                    avatar: "img/avatar_2.jpg",
-                    ultimoMessaggio: "ciao come stai ?",
-                    dataUltimoMessaggio: "10/08/2021 16:35",
-                    id: "chatUtente2"
+                    name: 'Fabio',
+                    avatar: '_2',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '20/03/2020 16:30:00',
+                            text: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            text: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'sent'
+                        }
+                    ],
                 },
                 {
-                    nome: "Paolo Fontana",
-                    avatar: "img/avatar_3.jpg",
-                    ultimoMessaggio: "ciao come stai ?",
-                    dataUltimoMessaggio: "10/08/2021 16:35",
-                    id: "chatUtente3"
+                    name: 'Samuele',
+                    avatar: '_3',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '28/03/2020 10:10:40',
+                            text: 'La Marianna va in campagna',
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            text: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            text: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],
                 },
                 {
-                    nome: "Michele Scotti",
-                    avatar: "img/avatar_4.jpg",
-                    ultimoMessaggio: "ciao come stai ?",
-                    dataUltimoMessaggio: "10/08/2021 16:35",
-                    id: "chatUtente4"
+                    name: 'Luisa',
+                    avatar: '_4',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
                 },
-                /* {
-                    nome: "Luigi Rossi",
-                    avatar: "img/avatar_5.jpg",
-                    ultimoMessaggio: "ciao come stai ?",
-                    dataUltimoMessaggio: "10/08/2021 16:35",
-                },
-                {
-                    nome: "Laura Mengoni",
-                    avatar: "img/avatar_6.jpg",
-                    ultimoMessaggio: "ciao come stai ?",
-                    dataUltimoMessaggio: "10/08/2021 16:35",
-                },
-                {
-                    nome: "Roberto Fontana",
-                    avatar: "img/avatar_7.jpg",
-                    ultimoMessaggio: "ciao come stai ?",
-                    dataUltimoMessaggio: "10/08/2021 16:35",
-                },
-                {
-                    nome: "Massimo Scotti",
-                    avatar: "img/avatar_8.jpg",
-                    ultimoMessaggio: "ciao come stai ?",
-                    dataUltimoMessaggio: "10/08/2021 16:35",
-                }, */
-
             ],
+
+
             newMessage: "",
             listaMessaggi: [
                 {
                     text: "Messaggio inviato",
-                    type: "sended",
-                    id: "chatUtente1"
+                    status: "sent",
+                    date: "10/10/10"
                 },
                 {
                     text: "Messaggio ricevuto",
-                    type: "recived",
-                    id: "chatUtente1"
+                    status: "received",
+                    date: "10/10/10"
                 }
-            ]
+            ],
+            activeChat: {},
 
+            messaggioTesto: "",
         },
         methods: {
             printMessage() {
@@ -82,6 +116,24 @@ window.addEventListener("DOMContentLoaded", function () {
                     type: "sended",
                 });
                 this.newMessage = "";
+            },
+            onClickChat(chat, i) {
+                this.activeChat = chat.messages;
+                this.listaMessaggi = [];
+                for (let i = 0; i < chat.messages.length; i++) {
+                    let messaggio = chat.messages[i].text;
+                    let statoMessaggio = chat.messages[i].status;
+                    let dataMessaggio = chat.messages[i].date;
+                    console.log(messaggio);
+                    console.log(statoMessaggio);
+                    this.listaMessaggi.push({
+                        date: dataMessaggio,
+                        text: messaggio,
+                        status: statoMessaggio,
+                    });
+                };
+
+
             }
         }
     });
